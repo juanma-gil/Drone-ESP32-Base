82b9254019b3b45377ef809c5a3f115f03aac4ff
sofia-am <sofiamallo07@hotmail.com>
2023-05-11 15:53:14 +0000

    feat: add esp32 board file

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 RMTT_RGB tt_rgb;
 RMTT_Protocol tt_sdk;
 
+//uint8_t io_map[][2] = {{5,21}}; //Agregar los pines que se quieran usar, [pin_mapeado, pin_a_mappear]
 void defaultCallback(char *cmd, String res)
 {
 	char msg[100];
@@ -25,28 +26,32 @@ void setup()
 	tt_rgb.Init();
 	tt_rgb.SetRGB(250, 0, 0);
 	Serial.begin(115200);
+	Serial.println("me conecte a la compu");
 	Serial1.begin(1000000, SERIAL_8N1, 23, 18);
-	 
- }
+	//pinMode(5, OUTPUT);
+	Serial.println("me conecte al dron");
+	Serial1.println("Hello World");
+}
 void loop()
 {
 	tt_rgb.SetRGB(0, 250, 0);
-	
+	//digitalWrite(5, HIGH);	
 	tt_sdk.startUntilControl(); // Esta funcion ejecuta el comando "command" que inicializa el sdk
-	//tt_sdk.sdkOn(defaultCallback);
+	//digitalWrite(5, LOW);
+	// tt_sdk.sdkOn(defaultCallback);
 	delay(1000);
-	tt_sdk.motorOn(defaultCallback);
-	delay(1000);
+	// tt_sdk.motorOn(defaultCallback);
+	// delay(1000);
 	tt_sdk.getBattery(defaultCallback);
 	delay(1000);
-	tt_sdk.getWiFi(defaultCallback);
-	tt_sdk.streamOn(defaultCallback); 
-	delay(1000);
-	tt_sdk.setResolution((char *)"low", defaultCallback);
-	delay(1000); 
-	tt_sdk.sendCmd((char *)"motoroff", defaultCallback); //Si se quiere enviar alguna función usando los strings
-	delay(1000);
-	tt_sdk.streamOff(defaultCallback);
+	// tt_sdk.getWiFi(defaultCallback);
+	// tt_sdk.streamOn(defaultCallback);
+	// delay(1000);
+	// tt_sdk.setResolution((char *)"low", defaultCallback);
+	// delay(1000);
+	// tt_sdk.sendCmd((char *)"motoroff", defaultCallback); // Si se quiere enviar alguna función usando los strings
+	// delay(1000);
+	// tt_sdk.streamOff(defaultCallback);
 	tt_rgb.SetRGB(0, 0, 250);
-	delay(5000);
+	delay(1000);
 }
