919ff7d52fc3dbeda088527c89a01e8b2dcd3a3b
sofia-am <sofiamallo07@hotmail.com>
2023-05-15 16:08:02 +0000

    feat
    * Implement ToF sensor
    * Implement tasks
    * Drone stop when an object is detected by the ToF

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,63 @@
 #include <RMTT_Libs.h>
-#include <WiFi.h>
+#include <Wire.h>
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
 #include <unistd.h>
 
 // Create an object
 RMTT_RGB tt_rgb;
 RMTT_Protocol tt_sdk;
+RMTT_TOF tt_sensor;
 
-//uint8_t io_map[][2] = {{5,21}}; //Agregar los pines que se quieran usar, [pin_mapeado, pin_a_mappear]
+TaskHandle_t sensorTaskHandle = NULL;
+TaskHandle_t mainTaskHandle = NULL;
+void sensor_task(void *arg);
+void main_task(void *arg);
 void defaultCallback(char *cmd, String res)
 {
 	char msg[100];
 	sprintf(msg, "The command was: %s\nRes: %s", cmd, res.c_str());
 	Serial.println(String(msg));
 }
+void mainTaskCallback(char *cmd, String res)
+{
+	xTaskCreateUniversal(sensor_task, "sensor_task", 8192, NULL, 1, &sensorTaskHandle, 0);
+}
+	
 
 // Main program start
 void setup()
 {
 	tt_rgb.Init();
-	tt_rgb.SetRGB(250, 0, 0);
-	Serial.begin(115200);
-	Serial.println("me conecte a la compu");
-	Serial1.begin(1000000, SERIAL_8N1, 23, 18);
-	//pinMode(5, OUTPUT);
-	Serial.println("me conecte al dron");
-	Serial1.println("Hello World");
-}
-void loop()
-{
-	tt_rgb.SetRGB(0, 250, 0);
-	//digitalWrite(5, HIGH);	
-	tt_sdk.startUntilControl(); // Esta funcion ejecuta el comando "command" que inicializa el sdk
-	//digitalWrite(5, LOW);
-	// tt_sdk.sdkOn(defaultCallback);
-	delay(1000);
-	// tt_sdk.motorOn(defaultCallback);
-	// delay(1000);
-	tt_sdk.getBattery(defaultCallback);
-	delay(1000);
-	// tt_sdk.getWiFi(defaultCallback);
-	// tt_sdk.streamOn(defaultCallback);
-	// delay(1000);
-	// tt_sdk.setResolution((char *)"low", defaultCallback);
-	// delay(1000);
-	// tt_sdk.sendCmd((char *)"motoroff", defaultCallback); // Si se quiere enviar alguna función usando los strings
-	// delay(1000);
-	// tt_sdk.streamOff(defaultCallback);
 	tt_rgb.SetRGB(0, 0, 250);
+	Serial.begin(115200);
+	Serial1.begin(1000000, SERIAL_8N1, 23, 18);
+
+	Wire.begin(27, 26);
+	Wire.setClock(100000);
+	tt_sensor.SetTimeout(500); // Set a single measurement timeout
+	if (!tt_sensor.Init())
+	{
+		Serial.println("Failed to detect and initialize sensor!");
+		while (1)
+		{
+		}
+	}
+}
+
+void loop(){
+	tt_sdk.startUntilControl();
 	delay(1000);
+	tt_sdk.takeOff(mainTaskCallback);
+}
+
+void sensor_task(void *arg)
+{
+	while (tt_sensor.ReadRangeSingleMillimeters() > 300)
+	{
+		delay(10);
+	}
+	tt_rgb.SetRGB(250, 0, 0);
+	tt_sdk.land(defaultCallback);
 }
